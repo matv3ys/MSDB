425c0f6ca06dfb604ea9acb1da70ea6722cd5112
matv3ys <matv3ys1337@gmail.com>
2025-05-11 22:11:04 -0800

    added structure

---
+++ include/libmsdb/libmsdb.hpp
@@ -0,0 +1,8 @@
+//
+// Created by matvey on 11.05.25.
+//
+
+#ifndef MSDB_LIBMSDB_HPP
+#define MSDB_LIBMSDB_HPP
+
+#endif //MSDB_LIBMSDB_HPP
---
+++ src/libmsdb.cpp
@@ -0,0 +1,3 @@
+//
+// Created by matvey on 11.05.25.
+//
